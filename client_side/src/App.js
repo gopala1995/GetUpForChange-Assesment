39b5b929865d8efd261c092d0a3fdab2513fa212
Gopala <guptaraja727@gmail.com>
2022-05-29 15:48:12 +0530

    last

--- client_side/src/App.js
+++ client_side/src/App.js
@@ -1,18 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { HomePage } from "./components/HomePage";
 import { SignUpPage } from "./components/SignUpPage";
-import { LoginPage } from "./components/LoginPage";
+import { Card } from "./components/Card.jsx";
+import React from "react";
+import Cookies from "js-cookie";
+import { Route, Switch } from "react-router-dom";
+import Dash from "./components/Dash";
+import { Link, Redirect } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-      {/* <SignUpPage/> */}
-      {/* Checking the code is commited or not */}
-      <LoginPage />
-    </div>
-  );
+  const [userData, setUserdata] = React.useState([]);
+
+  React.useEffect(() => {
+    const data = Cookies.get("user");
+    setUserdata(data && JSON.parse(data));
+  }, []);
+  if (userData?.isAuth) {
+    return <Dash />;
+  } else {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => <Card />} />
+          <Route path="/signup" render={() => <SignUpPage />} />
+          <Route path="/dash" render={() => <Dash />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
